0ba130e5dec8f8f43534bfa11afca6eaab1acc89
omele4ka <omele4ka@inbox.ru>
2023-07-24 11:51:56 -0500

    task 1 is solved

--- task_1.py
+++ task_1.py
@@ -2,3 +2,11 @@
 # с дублирующимися элементами. В результирующем списке
 # не должно быть дубликатов. [1, 2, 3, 1, 2, 4, 5] -> [1, 2]
 
+my_list = [1, 2, 3, 1, 2, 4, 5]
+duplicate_list = list()
+
+for item in my_list:
+    if my_list.count(item) > 1 and item not in duplicate_list:
+        duplicate_list.append(item)
+
+print(duplicate_list)
